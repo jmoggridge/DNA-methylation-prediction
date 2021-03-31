198fb50f1d7533698a8d55998533fd135d7e4e70
jmoggridge <ja.moggridge@gmail.com>
2021-03-31 00:29:03 -0400

    still getting weird python issues when running sbatch scripts

--- methylation_pipeline.sh
+++ methylation_pipeline.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-#SBATCH --time=0-03:00:00 ## days-hours:minutes:seconds
-#SBATCH --nodes=2
+#SBATCH --time=0-02:00:00 ## days-hours:minutes:seconds
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4 # number of threads
-#SBATCH --mem=24000 # requested memory (in MB)
-#SBATCH --mail-type=END
+#SBATCH --mem=20000 # requested memory (in MB)
 
 ### or: 
 # srun --pty --account "def-nricker" -t 0-03:00:00 --nodes=2 --mem=20G /bin/bash
@@ -14,6 +13,7 @@
 # setup modules in virtual environment in the project directory
 # SOURCEDIR=~/scratch/DNA_methylation
 
+echo "Loading modules and creating environment"
 # load python and scipy
 module load python/3.7.0
 module load scipy-stack
@@ -29,16 +29,21 @@ pip install --no-index matplotlib
 pip install --no-index seaborn 
 pip install --no-index xgboost 
 pip install pickle-mixin
+# to solve the "DataFrame object has no attribute 'to_numpy()" error
+pip3 install --upgrade pandas 
 
 ### Run these scripts in sequentially, separate batches
 # this script generates features and creates new csv files for X and y
 # python ./cpg_1_preprocessing.py
 
-# # this script does the model selection
-# python ./cpg_2_model_selection.py 
+echo "Starting python script ./cpg_2a_logistic_reg.py"
+
+# # these scripts do the modelling
+python ./cpg_2a_Logistic_reg.py
+
+echo "Finished script"
 
 
 deactivate
 
-
-
+echo "Deactivated environmnet"
