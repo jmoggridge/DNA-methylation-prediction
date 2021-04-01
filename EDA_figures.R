f665131c844883de1ca03d4378ab82797604c3e1
jmoggridge <ja.moggridge@gmail.com>
2021-04-01 00:23:45 -0400

    should be enough eda figures

--- EDA_figures.R
+++ EDA_figures.R
@@ -8,7 +8,7 @@ meth <- meth %>%
     chromosome = CHR,
     island = Relation_to_UCSC_CpG_Island,
     feature = Regulatory_Feature_Group,
-    methylated = ifelse(Beta==1, TRUE, FALSE)
+    methylated = ifelse(Beta==1, 'methylated', 'unmethylated')
   )
 meth <- meth %>% 
   mutate(across(everything(), ~ ifelse(is.na(.x), 'None', .x)))
@@ -21,8 +21,9 @@ fig1 <- meth %>%
   rcartocolor::scale_color_carto_d(palette = 2) +
   rcartocolor::scale_fill_carto_d(palette = 2) +
   theme_light() +
-  theme(legend.position = 'NA') +
-  labs(subtitle = 'Relation to CpG island', x = '', y ='')
+  theme(legend.position = c(0.7, 0.7)) +
+  labs(subtitle = 'Relation to CpG island',
+       x = '', y ='', color = '', fill = '')
 
 fig1
 write_rds(fig1, "./figures/fig_islands.rds")
@@ -40,7 +41,7 @@ fig2 <- meth %>%
   geom_col() +
   rcartocolor::scale_fill_carto_d(palette = 2) +
   theme_minimal() +
-  labs(subtitle = 'Chromosome', y = 'proportion of sites')
+  labs(subtitle = 'Chromosome', y = 'proportion of sites', fill = '')
 fig2
 write_rds(fig2, "./figures/fig_chromosomes.rds")
 rm(fig2)
@@ -54,7 +55,7 @@ meth <- meth %>%
     Id = Id,
     chromosome = CHR,
     UCSC_RefGene_Group,
-    methylated = ifelse(Beta==1, TRUE, FALSE)
+    methylated = ifelse(Beta==1, 'methylated', 'unmethylated')
   ) %>% 
   mutate(across(everything(), ~ ifelse(is.na(.x), 'None', .x)))
 
@@ -69,36 +70,116 @@ fig3 <- meth %>%
   geom_col() +
   rcartocolor::scale_fill_carto_d(palette = 2) +
   theme_minimal() +
-  labs(subtitle = 'RefGene tags', x = 'n CpG sites')
+  labs(subtitle = 'RefGene tags', x = 'n CpG sites', y='', fill ='')
 fig3  
-write_rds(fig3, "./results/fig_refgene_groups.rds")
+write_rds(fig3, "./figures/fig_refgene_groups.rds")
 
 rm(list = ls())
 
+
+###
 meth <- read_csv("./data/train.csv")
-meth %>% transmute(
+meth <- meth %>% transmute(
   Id, 
+  Forward_Sequence,
   seq,
-  Beta
+  methylated = ifelse(Beta==1, 'methylated', 'unmethylated'),
+  island = Relation_to_UCSC_CpG_Island
 )
 
-library
+
+# function generates sequence feature columns [(1-4)-mers] for classification
+generate_kmer_features <- function(df){
+  df <- as.data.frame(df)
+  df$seq <- Biostrings::DNAStringSet(df$seq)
+  features.df <- df %>%
+    cbind(
+      Biostrings::letterFrequency(df$seq, letters = c('A', 'C','G'),
+                                  as.prob = TRUE),
+      Biostrings::dinucleotideFrequency(df$seq, as.prob = TRUE),
+      Biostrings::trinucleotideFrequency(df$seq, as.prob = TRUE),
+      Biostrings::oligonucleotideFrequency(df$seq, 4, as.prob = TRUE)
+    ) %>%
+    dplyr::select(-seq) 
+  return(features.df)
+}
+
+meth_longseq_kmers <- meth %>% 
+  generate_kmer_features() %>% 
+  select(methylated, AA:TT)
+
+fig4 <- meth_longseq_kmers %>% 
+  pivot_longer(AA:TT, names_to = 'dinucleotide',
+               values_to = 'prop') %>% 
+  ggplot(aes(x = prop*100, color = methylated, fill = methylated)) +
+  geom_density(alpha = 0.1) +
+  facet_wrap(~ dinucleotide) +
+  rcartocolor::scale_fill_carto_d(palette = 2) +
+  labs(x= '% composition', color ='',
+       subtitle = "DNA composition over 2 kbp around CpG sites") +
+  theme_minimal() +
+  theme(panel.grid = element_blank(), 
+        axis.text.y.left = element_blank(),
+        axis.line.y = element_blank())
+fig4
+write_rds(fig4, "./figures/fig_dinucleotide.rds")
 
 
 
 
+meth_longseq_kmers %>% 
+  select(methylated, CG, CA, GT, island) %>% 
+  pivot_longer(CG:GT, names_to = 'dinucleotide',
+               values_to = 'prop') %>% 
+  ggplot(aes(x = prop*100, color = methylated, fill = methylated)) +
+  geom_density(alpha = 0.1) +
+  facet_grid(dinucleotide ~ island) +
+  rcartocolor::scale_fill_carto_d(palette = 2) +
+  xlim(0, 15) +
+  labs(x= '% composition', color ='',
+       subtitle = "DNA composition over 2 kbp around CpG sites") +
+  theme_minimal() +
+  theme(panel.grid = element_blank(), 
+        axis.text.y.left = element_blank(),
+        axis.line.y = element_blank())
 
 
+meth_longseq_kmers %>% 
+  select(CA, CG, TG, methylated, island) %>% 
+  mutate(CG_over_CA_TG = CG/(CA+TG)) %>% 
+  ggplot(aes(CG_over_CA_TG, color = methylated, fill = methylated)) +
+  geom_density(alpha = 0.1) +
+  labs(x= "CG/(CA+TG)", color = '', 
+       subtitle = "DNA composition over 2 kbp around CpG sites") +
+  theme_minimal() +
+  facet_wrap(~island)
+  
+  
+
+meth_shortseq_kmers <- 
+  meth %>% 
+  transmute(island,
+            seq = Forward_Sequence,
+            methylated) %>% 
+  mutate(seq = str_remove_all(seq, '\\[|\\]')) %>% 
+  generate_kmer_features() %>% 
+  tibble() %>% 
+  select(island, methylated, AA:TT) 
 
 
-
-
-
-
-
-
-
-
+meth_shortseq_kmers %>% 
+  pivot_longer(AA:TT, names_to = 'dinucleotide',
+               values_to = 'prop') %>% 
+  ggplot(aes(x = prop*100, color = methylated, fill = methylated)) +
+  geom_histogram(alpha = 0.1, position = 'dodge') +
+  facet_wrap(~ dinucleotide, scales = 'free') +
+  rcartocolor::scale_fill_carto_d(palette = 2) +
+  labs(x= '% composition', 
+       subtitle = "DNA composition over 120 kbp around CpG sites") +
+  theme_minimal() +
+  theme(panel.grid = element_blank(), 
+        axis.text.y.left = element_blank(),
+        axis.line.y = element_blank())
 
 
 
