583574a5d28a879b4cfb595caeba94ebe752273b
jmoggridge <ja.moggridge@gmail.com>
2021-04-07 17:38:15 -0400

    got most of an introduction done, guess I should finish the analysis now eh

--- methylation_pipeline.sh
+++ methylation_pipeline.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem=20000 # requested memory (in MB)
 
 ### or: 
-# srun --pty --account "def-nricker" -t 0-03:00:00 --nodes=2 --mem=20G /bin/bash
+srun --pty --account "def-nricker" -t 0-02:00:00 --nodes=1 --ntasks-per-node=8 --mem=20G /bin/bash
 
 # First, create the virtual environment for running cpg_* scripts
 
@@ -15,21 +15,21 @@
 
 echo "Loading modules and creating environment"
 # load python and scipy
-module load python/3.7.0
+module load python #/3.7.0 doesn't work anymore?
 module load scipy-stack
 
-virtualenv --no-download $SLURM_TMPDIR/env
-source $SLURM_TMPDIR/env/bin/activate
+virtualenv --no-download ~/scratch/DNA_methylation/env
+source ~/scratch/DNA_methylation/env/bin/activate
 
-# install packages (happens everytime login node is used)
+# install packages (seem to need to do this each time compute node is used)
 pip install --no-index --upgrade pip
-pip install --no-index scikit-learn
+pip install --no-index numpy
 pip install --no-index pandas 
-pip install --no-index matplotlib 
-pip install --no-index seaborn 
-pip install --no-index xgboost 
+pip install --no-index scikit-learn
 pip install pickle-mixin
+
 # to solve the "DataFrame object has no attribute 'to_numpy()" error
+pip install pandas 
 pip3 install --upgrade pandas 
 
 ### Run these scripts in sequentially, separate batches
@@ -47,3 +47,15 @@ echo "Finished script"
 deactivate
 
 echo "Deactivated environmnet"
+
+
+
+
+# pip install --no-index matplotlib 
+# pip install --no-index seaborn 
+# pip install --no-index xgboost 
+
+
+# cp jmoggrid@cedar.computecanada:/scratch/jmoggrid/DNA_methylation/results/* ./results/  
+
+## reply from computecanada help - don't load the numpy module. just do packages with pip...
